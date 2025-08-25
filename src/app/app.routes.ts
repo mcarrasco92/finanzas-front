896cd10d9c3a846ee9cf810d20026a4c63ed6463
Mauricio Carrasco Ayala <mauriciocarrascoayala@MacBook-Air-de-Mauricio.local>
2025-08-25 13:49:51 -0600

    Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Registro } from './components/registro/registro';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Cuentas } from './components/cuentas/cuentas';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'registro', component: Registro}, // Ruta para el componente de registro
+  {path: 'dashboard', component: Dashboard, children: [
+    {path: 'cuentas', component: Cuentas}
+  ]}, // Ruta para el componente de dashboard
+  {path: '', component: Login}, // Ruta por defecto que carga el componente de login 
+  { path: '**', redirectTo: '' } // Redirige cualquier ruta no encontrada al login
+];
